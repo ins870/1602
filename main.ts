55b123c2193a2b6a68e81af7bdbd9292242e49ea
INS870 <116519264+ins870@users.noreply.github.com>
2023-10-31 17:54:49 +0100

    Aggiorna pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
-I2C_LCD1602.LcdInit(39)
+servos.P0.setAngle(0)
+I2C_LCD1602.LcdInit(24)
+JoyPiAdvanced.rfidInit()
 basic.forever(function () {
-    I2C_LCD1602.ShowString("Hello", 0, 0)
-    I2C_LCD1602.ShowString("Hello", 0, 1)
+    if (input.buttonIsPressed(Button.A)) {
+        servos.P0.setAngle(89)
+        basic.showNumber(5)
+        basic.pause(1000)
+        basic.showNumber(4)
+        basic.pause(1000)
+        basic.showNumber(3)
+        basic.pause(1000)
+        basic.showNumber(2)
+        basic.pause(1000)
+        basic.showNumber(1)
+        basic.pause(1000)
+        servos.P0.setAngle(0)
+    } else {
+        I2C_LCD1602.ShowString("NON AUTORIZZATO", 0, 1)
+    }
 })
